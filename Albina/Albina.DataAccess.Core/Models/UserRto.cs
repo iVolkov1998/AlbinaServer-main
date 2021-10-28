6e59118ba5f43e410e35a8ecd6e7e1e1576ccf70
Zhuludkovr <82827220+iVolkov1998@users.noreply.github.com>
2021-10-28 19:18:55 +0300

    Добавили Using и Int

---
+++ Albina/Albina.DataAccess.Core/Models/UserRto.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Albina.DataAccess.Core.Models
+{
+    [Table("User")]
+    class UserRto
+    {
+        [Key] public int Id { get; set; }
+
+        public string  { get; set; }
+
+
+
+    }
+}
